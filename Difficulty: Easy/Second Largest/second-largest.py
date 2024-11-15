64359e2e1c6811151e1e8578c3fe0ed70e9950af
Pranav krishna U <97517105+amenu4aie2@users.noreply.github.com>
2024-11-15 09:19:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Second Largest/second-largest.py
+++ Difficulty: Easy/Second Largest/second-largest.py
@@ -1,17 +1,16 @@
 #User function Template for python3
 class Solution:
-    def print2largest(self, arr):
-        # Code Here
-        l1,l2=float('-inf'),float('-inf')
+    def getSecondLargest(self, arr):
+        max1=-1
+        max2=-1
         for i in arr:
-            if(l1<i):
-                l2=l1
-                l1=i
-            elif(l2<i and i!=l1):
-                l2=i
+            if(i>max1):
+                max2=max1
+                max1=i
                 
-            
-        return l2 if l2!=float('-inf') else -1
+            elif(i>max2 and i!=max1):
+                max2=i
+        return max2
                 
 
 
@@ -24,7 +23,7 @@ if __name__ == "__main__":
     for _ in range(t):
         arr = list(map(int, input().split()))
         ob = Solution()
-        ans = ob.print2largest(arr)
+        ans = ob.getSecondLargest(arr)
         print(ans)
-
+        print("~")
 # } Driver Code Ends
