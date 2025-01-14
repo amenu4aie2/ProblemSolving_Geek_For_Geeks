7b0a2751d8273a00de344f1d9f18f9d021398852
Pranav krishna U <97517105+amenu4aie2@users.noreply.github.com>
2025-01-14 22:22:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Equilibrium Point/equilibrium-point.py
+++ Difficulty: Easy/Equilibrium Point/equilibrium-point.py
@@ -1,28 +1,28 @@
 # User function Template for python3
 
 class Solution:
-    # Complete this function
     #Function to find equilibrium point in the array.
-    def equilibriumPoint(self,arr):
-        s1,s2=[0]*len(arr),[0]*len(arr)
-        s1[0]=arr[0]
-        for i in range(1,len(arr)):
-            s1[i]=arr[i]+s1[i-1]
-        x=arr[::-1].copy()
-        s2[0]=x[0]
-        for i in range(1,len(x)):
-            s2[i]=x[i]+s2[i-1]
-        s2=s2[::-1]
+    def findEquilibrium(self, arr):
+        total_sum = sum(arr)
+        left_sum = 0
+        
         for i in range(len(arr)):
-            if(s2[i]==s1[i]):
-                return i+1
+            total_sum -= arr[i]
+            
+            if left_sum == total_sum:
+                return i
+            
+            left_sum += arr[i]
+        
         return -1
+
+
+
 
 
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
-
 import math
 
 
@@ -34,8 +34,8 @@ def main():
 
         ob = Solution()
 
-        print(ob.equilibriumPoint(arr))
-
+        print(ob.findEquilibrium(arr))
+        print("~")
         T -= 1
 
 
